88523d06136944956dbb5c7c8e426ff145405e84
Mark <stylite1991@gmail.com>
2017-07-05 21:02:19 +0800

    add java code
    add a empty java to show signature

---
+++ app/src/main/java/lzj/com/jnitest/CheckSignatureHelper.java
@@ -0,0 +1,33 @@
+package lzj.com.jnitest;
+
+/**
+ * Created by Mark on 2017/7/5.
+ */
+
+public class CheckSignatureHelper {
+   /* String getSignature() throws Exception{
+        Application application = ActivityThread.currentApplication();
+        PackageManager manager = application.getPackageManager();
+        String packageName = application.getPackageName();
+        PackageInfo packageInfo = manager.getPackageInfo(packageName,PackageManager.GET_SIGNATURES);
+        Signature signature = packageInfo.signatures[0];
+        byte[] chars = signature.toByteArray();
+        MessageDigest digest = MessageDigest.getInstance("md5");
+        digest.update(chars);
+        byte[] signArray = digest.digest(chars);
+
+        jsize intArrayLength = env->GetArrayLength(objArraySign);
+        jbyte *byte_array_elements = env->GetByteArrayElements(objArraySign, NULL);
+        size_t length = (size_t) intArrayLength * 2 + 1;
+    char *char_result = (char *) malloc(length);
+        memset(char_result, 0, length);
+
+        // 将byte数组转换成16进制字符串，发现这里不用强转，jbyte和unsigned char应该字节数是一样的
+        ByteToHexStr((const char *) byte_array_elements, char_result, intArrayLength);
+        // 在末尾补\0
+    *(char_result + intArrayLength * 2) = '\0';
+
+        jstring stringResult = env->NewStringUTF(char_result);
+    }
+         */
+}
